a9f5a8a6f3771e08c7f7357219c96928e3206c7d
Rishabh3993 <rishabhsingla1994@gmail.com>
2017-07-05 22:27:18 +0530

    adding function to like , comment and delete the negative post

--- instabot.py
+++ instabot.py
@@ -111,10 +111,81 @@ def get_follower_user_post(insta_username):
   else:
     print 'Status code other than 200 received!'
 
+def get_folower_post_id(insta_username):
+  user_id = get_follower_user_id(insta_username)
+  if user_id == None:
+    print 'User does not exist!'
+    exit()
+  request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+  print 'GET request url : %s' % (request_url)
+  user_media = requests.get(request_url).json()
 
-# calling self_information function
-self_information()
-get_follower_user_id('insta.mriu.test.0')
-get_follower_user_information('insta.mriu.test.0')
-get_own_post()
-get_follower_user_post('insta.mriu.test.0')
+  if user_media['meta']['code'] == 200:
+    if len(user_media['data']):
+      return user_media['data'][0]['id']
+    else:
+      print 'There is no recent post of the user!'
+      exit()
+  else:
+    print 'Status code other than 200 received!'
+    exit()
+
+def like_post(insta_username):
+  media_id = get_follower_post_id(insta_username)
+  request_url = (BASE_URL + 'media/%s/likes') % (media_id)
+  payload = {"access_token": APP_ACCESS_TOKEN}
+  print 'POST request url : %s' % (request_url)
+  post_a_like = requests.post(request_url, payload).json()
+  if post_a_like['meta']['code'] == 200:
+    print 'Like was successful!'
+  else:
+    print 'Your like was unsuccessful. Try again!'
+
+def post_comment(insta_username):
+  media_id = get_follower_post_id(insta_username)
+  comment_text = raw_input("Your comment: ")
+  payload = {"access_token": APP_ACCESS_TOKEN, "text": comment_text}
+  request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+  print 'POST request url : %s' % (request_url)
+
+  make_comment = requests.post(request_url, payload).json()
+
+  if make_comment['meta']['code'] == 200:
+    print "Successfully added a new comment!"
+  else:
+    print "Unable to add comment. Try again!"
+
+
+def negative_comment_delete(insta_username):
+    media_id = get_follower_post_id(insta_username)
+    request_url = (BASE_URL + 'media/%s/comments/?access_token=%s') % (media_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    comment_info = requests.get(request_url).json()
+
+    if comment_info['meta']['code'] == 200:
+        if len(comment_info['data']):
+            #Here's a naive implementation of how to delete the negative comments :)
+            for x in range(0, len(comment_info['data'])):
+                comment_id = comment_info['data'][x]['id']
+                comment_text = comment_info['data'][x]['text']
+                blob = TextBlob(comment_text, analyzer=NaiveBayesAnalyzer())
+                if (blob.sentiment.p_neg > blob.sentiment.p_pos):
+                    print 'Negative comment : %s' % (comment_text)
+                    delete_url = (BASE_URL + 'media/%s/comments/%s/?access_token=%s') % (media_id, comment_id, APP_ACCESS_TOKEN)
+                    print 'DELETE request url : %s' % (delete_url)
+                    delete_info = requests.delete(delete_url).json()
+
+                    if delete_info['meta']['code'] == 200:
+                        print 'Comment successfully deleted!\n'
+                    else:
+                        print 'Unable to delete comment!'
+                else:
+                    print 'Positive comment : %s\n' % (comment_text)
+        else:
+            print 'There are no existing comments on the post!'
+    else:
+        print 'Status code other than 200 received!'
+
+
+
+
